e873bffe184134a0d73a6829c24afebb9d5b06ef
Bire11 <birhanuwoeldmariam@gmail.com>
2023-06-04 02:46:11 -0700

    server two

---
+++ new/peractis.js
@@ -0,0 +1,10 @@
+function add(x,y){
+    return x+y;
+
+}
+const res =add (3,2)
+
+console.log(res);
+// console.log("the first node js");
+
+
--- react/index.js
+++ react/index.js
@@ -4,12 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Bmi from './components/bmi';
-
+import AjaxApi from './components/AjaxApi';
+import ServerTwo from './components/ServerTwo';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
-    <Bmi/>
+    {/* <App /> */}
+    {/* <Bmi/> */}
+    {/* <AjaxApi/> */}
+    < ServerTwo/>
   </React.StrictMode>
 );
 
